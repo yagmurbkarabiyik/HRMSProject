a032a5b400ea1755c399cbac16f797c5d4584aa7
yagmurbkarabiyik <83311035+yagmurbkarabiyik@users.noreply.github.com>
2021-05-11 19:06:59 +0300

    initial commit

---
+++ src/main/java/kodlamaio/hrms/dataAccess/abstracts/JobTitleDao.java
@@ -0,0 +1,8 @@
+package kodlamaio.hrms.dataAccess.abstracts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import kodlamaio.hrms.entities.concretes.JobTitle;
+
+public interface JobTitleDao extends JpaRepository<JobTitle, Integer>{
+
+	
+}
